23856f12dc8673d4d54365c434a13ab087b14d37
Pratik <j.pratik025@gmail.com>
2024-04-27 12:09:51 +0530

    minor updated (DetailActivity)

--- app/src/main/java/com/example/opalwish/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/example/opalwish/ui/activity/DetailActivity.kt
@@ -2,9 +2,8 @@ package com.example.opalwish.ui.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -15,7 +14,6 @@ import com.example.opalwish.data.ProductModel
 import com.example.opalwish.room_database.AppDatabase
 import com.example.opalwish.room_database.RoomDao
 import com.example.opalwish.room_database.RoomProductModel
-import com.example.opalwish.ui.fragment.DashboardFragment
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -29,12 +27,15 @@ class DetailActivity : AppCompatActivity() {
         com.example.opalwish.databinding.ActivityDetailBinding.inflate(layoutInflater)
     }
     private var productModel = ProductModel()
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val productId = intent.getStringExtra("PRODUCT_ID")
+
+        binding.detailShimmer.startShimmer()
         binding.productImage.load(R.drawable.image_loader)
 
         if (productId != null) {
@@ -42,24 +43,26 @@ class DetailActivity : AppCompatActivity() {
             Firebase.firestore.collection("Products").document(productId).get()
                 .addOnSuccessListener {
 
-                        @Suppress("DEPRECATION")
-                        productModel = it.toObject<ProductModel>()!!
-                        productModel.product_id = it.id
+                    @Suppress("DEPRECATION")
+                    productModel = it.toObject<ProductModel>()!!
+                    productModel.product_id = it.id
 
-                        val imageUrl = productModel.imageUrl
-                        binding.productImage.load(imageUrl)
+                    val imageUrl = productModel.imageUrl
+                    binding.productImage.load(imageUrl)
 
-                        binding.productName.text = productModel.name
-                        binding.productDesc.text = productModel.disp
-                        binding.productDetails.text = productModel.details
-                        binding.productPrice.text = "₹ "+ productModel.price.toString()
+                    binding.productName.text = productModel.name
+                    binding.productDesc.text = productModel.disp
+                    binding.productDetails.text = productModel.details
+                    binding.productPrice.text = "₹ "+ productModel.price.toString()
 
-                        val name = productModel.name
-                        val disp = productModel.disp
-                        val price = productModel.price
-                        if (imageUrl != null) {
-                            cartAction(productId,name,disp,price,imageUrl)
-                        }
+                    binding.detailShimmer.visibility = View.GONE
+                    binding.detailInfo.visibility = View.VISIBLE
+                    val name = productModel.name
+                    val disp = productModel.disp
+                    val price = productModel.price
+                    if (imageUrl != null) {
+                        cartAction(productId,name,disp,price,imageUrl)
+                    }
 
                 }
                 .addOnFailureListener { exception ->
@@ -92,22 +95,22 @@ class DetailActivity : AppCompatActivity() {
 
     private fun cartAction(productId: String, name: String?, disp: String?, price: Double?, image: String) {
 
-        val RoomDao = AppDatabase.getInstance(this).RoomDao()
+        val dao = AppDatabase.getInstance(this).RoomDao()
 
-        if(RoomDao.isExit(productId) != null){
+        if(dao.isExit(productId) != null){
             binding.btnAddToCart.text = "Go To Cart"
         }else{
             binding.btnAddToCart.text = "Add to Cart"
         }
 
         binding.btnAddToCart.setOnClickListener {
-            if(RoomDao.isExit(productId) != null){
+            if(dao.isExit(productId) != null){
                 openCart()
             }else{
                 binding.btnAddToCart.backgroundTintList = ContextCompat.getColorStateList(this, R.color.theme_color)
 
                 Toast.makeText(this, "Item added to Cart", Toast.LENGTH_SHORT).show()
-                addToCart(RoomDao,productId,name,disp,price,image)
+                addToCart(dao,productId,name,disp,price,image)
             }
         }
     }
